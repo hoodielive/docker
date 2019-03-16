f11394604963c2140aae99513e23626b2b6853fa
hoodielive <hoodielive@gmail.com>
2019-03-16 15:08:35 -0400

    $

---
+++ simpleweb/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); 
+
+const app = express(); 
+
+app.get('/', (req, res) => {
+    res.send('Howdy!'); 
+}); 
+
+const port = 8080; 
+
+app.listen(port, () => {
+   console.log('Listening on port 8080...');  
+}); 
