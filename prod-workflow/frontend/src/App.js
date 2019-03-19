fe3d91ae732a455c73935c82d3adeaf4b6028e8e
hoodielive <hoodielive@gmail.com>
2019-03-18 20:57:07 -0400

    make docker-compose.yml configuration

--- prod-workflow/frontend/src/App.js
+++ prod-workflow/frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-	    Howdy!
+	    Howdy! I will be updated automatically shortly thanks to Docker volumes -v lol
           </p>
           <a
             className="App-link"
