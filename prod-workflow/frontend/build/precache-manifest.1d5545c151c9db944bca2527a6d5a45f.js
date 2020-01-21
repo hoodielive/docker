584618ef3933d8385653456ed2aeaa8eecc8c8bd
hoodielive <hoodielive@gmail.com>
2020-01-21 14:04:35 -0500

    Understand Network Namespaces notes

---
+++ prod-workflow/frontend/build/precache-manifest.1d5545c151c9db944bca2527a6d5a45f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1c4e1483e2788b1bbe2",
+    "url": "/static/css/main.15419c13.chunk.css"
+  },
+  {
+    "revision": "e1c4e1483e2788b1bbe2",
+    "url": "/static/js/main.9b36efc2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "18b05a69b72782e7ba33a81a60ccfabf",
+    "url": "/index.html"
+  }
+];
